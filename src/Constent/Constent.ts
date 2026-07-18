5835c75d39946da0737d600686df2cdd1615d5f4
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-07-18 23:23:34 +0530

    Url Changed

--- src/Constent/Constent.ts
+++ src/Constent/Constent.ts
@@ -1,5 +1,5 @@
-export const API_BASE="http://localhost:5000"
+// export const API_BASE="http://localhost:5000"
 
 
 
-// export const API_BASE="https://kuttypapacrationbackend.vercel.app"
+export const API_BASE="https://kuttypapacrationbackend.vercel.app"
