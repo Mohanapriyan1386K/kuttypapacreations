388893d357662befa4ab5234c82bd82c272118bf
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-07-18 21:00:11 +0530

    fixed

--- src/Screen/Product.tsx
+++ src/Screen/Product.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { Category, Product } from "../type";
 import { useDispatch } from "react-redux";
 import { openModal } from "../Redux/Reducers/ModalReducer";
